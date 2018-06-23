257d76a9f0e33d2664034c11d0e0582095e2070f
boundsj <jesse@rebounds.net>
2018-06-23 14:37:20 -0700

    Organize code into “bot” and “bot library”

--- Sources/clockbot/main.swift
+++ Sources/clockbot/main.swift
@@ -1,10 +1,15 @@
 import Foundation
 import WebSocket
 
+// Bot library
+
 class HTTPService {
 
+    static let rtmConnectURLTemplate = "https://slack.com/api/rtm.connect?token=%@"
+
     class func connect(token: String, completion: @escaping (_ websocketString: String) -> () ) {
-        let url = URL(string: "https://slack.com/api/rtm.connect?token=\(token)")!
+        let urlString = String(format: rtmConnectURLTemplate, token)
+        let url = URL(string: urlString)!
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data else {
                 fatalError()
@@ -30,70 +35,89 @@ class HTTPService {
 
 }
 
+public class SlackRTMClient {
+
+    let token: String
+
+    private var messageId = 1
+    private let worker = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+    private var websocket: WebSocket?
+
+    init(token: String) {
+        self.token = token
+    }
+
+    func start() {
+
+        HTTPService.connect(token: token) { websocketString in
+            print("Connecting with: \(websocketString)")
+
+            // Parse the host and path for connection received from rtm connect
+            let socket = "/websocket/"
+            var hostname = websocketString.components(separatedBy: socket)[0]
+            hostname = hostname[6..<hostname.count]
+            let path = websocketString.components(separatedBy: socket)[1]
+
+            do {
+                self.websocket = try HTTPClient.webSocket(scheme: .wss,
+                                                          hostname: hostname,
+                                                          path: "\(socket)\(path)",
+                                                          on: self.worker).wait()
+            } catch {
+                fatalError("Unexpected error when creating websocket")
+            }
+
+            guard let websocket = self.websocket else {
+                fatalError("Websocket is not available!")
+            }
+
+            websocket.onText { ws, text in
+                print(text)
+                if text == "{\"type\": \"hello\"}" { return }
+
+                if text.contains("time") {
+                    var response: Dictionary = [String: Any]()
+                    response["type"] = "message"
+                    response["channel"] = "C03BMRQLA"
+                    response["text"] = "Hello, it is \(Date().description)"
+                    response["id"] = self.messageId
+
+                    let responseJSON = try! JSONSerialization.data(withJSONObject: response)
+                    let string = String(data: responseJSON, encoding: String.Encoding.utf8)!
+                    print("=====> sending: \(string)")
+                    ws.send(text: string)
+                    self.messageId += 1
+                }
+            }
+
+            websocket.onCloseCode { code in
+                print("code: \(code)")
+            }
+
+            websocket.onError { (_, error) in
+                print(error)
+            }
+        }
+
+    }
+}
+
 extension String {
+
     subscript(_ range: CountableRange<Int>) -> String {
         let idx1 = index(startIndex, offsetBy: max(0, range.lowerBound))
         let idx2 = index(startIndex, offsetBy: min(self.count, range.upperBound))
         return String(self[idx1..<idx2])
     }
+
 }
 
-var messageId = 1
-let worker = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+/// Bot
 
 guard let token = ProcessInfo.processInfo.environment["token"] else {
     fatalError("Could not find a token!")
 }
 
-HTTPService.connect(token: token) { websocketString in
-    print("Connecting with: \(websocketString)")
-
-    let socket = "/websocket/"
-    var hostname = websocketString.components(separatedBy: socket)[0]
-    hostname = hostname[6..<hostname.count]
-    let path = websocketString.components(separatedBy: socket)[1]
-
-    let ws = try! HTTPClient.webSocket(scheme: .wss,
-                                       hostname: hostname,
-                                       path: "\(socket)\(path)",
-                                       on: worker).wait()
-
-    ws.onText { wsIn, text in
-        print(text)
-        if text == "{\"type\": \"hello\"}" { return }
-
-        if text.contains("time") {
-            var response: Dictionary = [String: Any]()
-            response["type"] = "message"
-            response["channel"] = "C03BMRQLA"
-            response["text"] = "Hello, it is \(Date().description)"
-            response["id"] = messageId
-            
-            let responseJSON = try! JSONSerialization.data(withJSONObject: response)
-            let string = String(data: responseJSON, encoding: String.Encoding.utf8)!
-            print("=====> sending: \(string)")
-            wsIn.send(text: string) 
-            messageId = messageId + 1
-        }
-    }
-
-    ws.onCloseCode { code in
-        print("code: \(code)")
-    }
-
-    ws.onError { (_, error) in
-        print(error)
-    }
-}
-
+let rtm = SlackRTMClient(token: token)
+rtm.start()
 dispatchMain()
-
-//ws.close(code: .normalClosure)
-
-//let promise = worker.eventLoop.newPromise(String.self)
-//promise.succeed(result: text)
-
-//var loopUntil = Date(timeIntervalSinceNow: 0.1)
-//while true && RunLoop.current.run(mode: .defaultRunLoopMode, before: loopUntil) {
-//    loopUntil = Date(timeIntervalSinceNow: 0.1)
-//}
